4f6645a88f775aca5b854cf8f5d291e3f94ab6e5
zrohac <104515820+zrohac@users.noreply.github.com>
2022-04-27 18:40:42 +0200

    debugging

--- app.py
+++ app.py
@@ -2,9 +2,14 @@
 A sample Hello World server.
 """
 import os
-
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS
+
+import numpy as np
+import pandas as pd
+import pickle
+import io
+from sklearn.preprocessing import StandardScaler
 
 # pylint: disable=C0103
 app = Flask(__name__)
@@ -25,9 +30,38 @@ def hello():
         Revision=revision)
 
 
-@app.route('/check-transaction')
+@app.route('/check')
 def check():
-    result = {"result":"OK"}
+
+    inputLine="4462,-2.30334956758553,1.759247460267,-0.359744743330052,2.33024305053917,-0.821628328375422,-0.0757875706194599,0.562319782266954,-0.399146578487216,-0.238253367661746,-1.52541162656194,2.03291215755072,-6.56012429505962,0.0229373234890961,-1.47010153611197,-0.698826068579047,-2.28219382856251,-4.78183085597533,-2.61566494476124,-1.33444106667307,-0.430021867171611,-0.294166317554753,-0.932391057274991,0.172726295799422,-0.0873295379700724,-0.156114264651172,-0.542627889040196,0.0395659889264757,-0.153028796529788,239.93"
+
+    modelFileName="storedModel.pckl"
+
+    # load the model from disk
+    loaded_model = pickle.load(open(modelFileName, 'rb'))
+
+    # Header line
+    csvHeader="""Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount
+    """
+
+    # Create datafile
+    TESTDATA=csvHeader+"\n"+inputLine
+    df = pd.read_csv(io.StringIO(TESTDATA), sep=",")
+
+    # Prepare scaler
+    sc=StandardScaler()
+    sc.set_params(**{'copy': True, 'with_mean': True, 'with_std': True})
+
+    # Transform ammount column
+    df['normAmount'] = sc.fit_transform(df['Amount'].values.reshape (-1,1))
+
+    # Remove not needed columns
+    df = df.drop (['Time', 'Amount'], axis = 1);
+
+    # load the model from disk
+    loaded_model = pickle.load(open(modelFileName, 'rb'))
+    result1 = loaded_model.predict(df.iloc[[0]])[0]
+    result = {"result":'OK' if result1 == 0 else 'FRAUD'}
     return jsonify(result)
         
 
